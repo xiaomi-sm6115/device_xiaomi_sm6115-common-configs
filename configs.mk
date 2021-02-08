6b08fbb877e46920dd5e2b1c2ce77528d1514ac5
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-08 09:59:07 +0100

    sm6115-common: Import audio configs
    * From lime-user 11 V12.9.5.0.RJQMIXM release-keys
    
    Change-Id: Ie2f0337ff2bfff9d6a569920c311e38fabeb387a

---
+++ configs.mk
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2023 SM6115-common Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio_effects.xml \
+    audio_io_policy.conf \
+    audio_log.cfg \
+    audio_platform_info.xml \
+    audio_platform_info_idp.xml \
+    audio_platform_info_intcodec.xml \
+    audio_platform_info_scubaidp.xml \
+    audio_platform_info_scubaqrd.xml \
+    audio_policy_configuration.xml \
+    audio_policy_volumes.xml \
+    mixer_paths.xml \
+    mixer_paths_idp.xml \
+    mixer_paths_scubaidp.xml \
+    mixer_paths_scubaqrd.xml \
+    sound_trigger_mixer_paths.xml \
+    sound_trigger_mixer_paths_idp.xml \
+    sound_trigger_mixer_paths_scubaidp.xml \
+    sound_trigger_mixer_paths_scubaqrd.xml \
+    sound_trigger_platform_info.xml
