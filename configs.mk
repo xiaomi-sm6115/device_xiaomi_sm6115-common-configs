b779ba479b2be3c933988d5d2f837b5a05a4fc43
I-n-o-k <inok.dr189@gmail.com>
2023-08-21 20:52:34 +0700

    sm6115-common: media: Update config from lime 14.0.5.0.SJQMIXM

--- configs.mk
+++ configs.mk
@@ -32,11 +32,14 @@ PRODUCT_PACKAGES += \
     media_codecs_performance.xml \
     media_codecs_performance_v1.xml \
     media_codecs_performance_v2.xml \
+    media_codecs_performance_v3.xml \
     media_codecs_vendor.xml \
     media_codecs_vendor_audio.xml \
     media_codecs_vendor_v1.xml \
     media_codecs_vendor_v2.xml \
+    media_codecs_vendor_v3.xml \
     media_profiles.xml \
+    media_profiles_khaje.xml \
     media_profiles_scuba.xml \
     media_profiles_vendor.xml \
     system_properties.xml
